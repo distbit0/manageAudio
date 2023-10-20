71f62a36f8f0bc65fcabb3be829c8006ec7497da
Dist Bit <adp01@pm.me>
2023-10-20 20:02:56 +1100

    audio_monitor.sh nohup.out toggleAudioSink.py

--- audio_monitor.sh
+++ audio_monitor.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Get the script's name
+SCRIPT_NAME="$(basename "$0")"
+
+# Get the PID of the current script
+SCRIPT_PID="$$"
+
+# Kill all existing instances of the script except for the current one
+for pid in $(pgrep -f $SCRIPT_NAME); do
+    if [ $pid != $SCRIPT_PID ]; then
+        kill -9 $pid
+    fi
+done
+
 while true; do
     AUDIO_STREAMS=$(pacmd list-sink-inputs | grep 'state: RUNNING')
 
---
+++ toggleAudioSink.py
@@ -0,0 +1,52 @@
+import os
+
+
+def get_sinks():
+    os.system("pactl list sinks short > tmp_sinks.txt")
+    sinks = []
+    with open("tmp_sinks.txt", "r") as f:
+        lines = f.readlines()
+    for line in lines:
+        sink_id, name = line.split()[0], line.split()[1]
+        sinks.append({"id": sink_id, "name": name})
+    os.remove("tmp_sinks.txt")
+    return sinks
+
+
+def get_default_sink():
+    os.system("pactl info > tmp_info.txt")
+    with open("tmp_info.txt", "r") as f:
+        lines = f.readlines()
+    for line in lines:
+        if "Default Sink: " in line:
+            return line.split(": ")[1].strip()
+    os.remove("tmp_info.txt")
+
+
+def set_default_sink(sink_name):
+    os.system(f"pactl set-default-sink {sink_name}")
+
+
+def main():
+    sinks = get_sinks()
+    default_sink = get_default_sink()
+
+    bluetooth_sink = None
+    speaker_sink = None
+
+    for sink in sinks:
+        if "bluez" in sink["name"]:
+            bluetooth_sink = sink["name"]
+        else:
+            speaker_sink = sink["name"]
+
+    if default_sink == bluetooth_sink:
+        set_default_sink(speaker_sink)
+        print("Switched to Speaker.")
+    else:
+        set_default_sink(bluetooth_sink)
+        print("Switched to Bluetooth.")
+
+
+if __name__ == "__main__":
+    main()
